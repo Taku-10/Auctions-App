62c1a0c21a5452b6615092ad0fd02ee60534890d
Takudzwa Maseva <takudzwamaseva020@gmail.com>
2023-02-14 00:49:37 +0200

    Updated flash and session configurations

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const path = require("path");
+// const catchAsync = require("./utilities/catchAsync");
+// const AppError = require("./utilities/AppError");
+// const Joi = require("joi")
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const Bid = require("./models/bid");
@@ -8,45 +11,41 @@ const listingRoutes = require("./routes/listingsRoutes");
 const bidRoutes = require("./routes/bidsRoutes");
 const session = require("express-session");
 const flash = require("connect-flash");
-// const Campground = require("./models/campground");
 const ejsMate = require("ejs-mate");
-// const catchAsync = require("./Utilities/catchAsync");
-// const AppError = require("./Utilities/AppError");
-// const Joi = require("joi");
-// const {campgroundSchema} = require("./schemas.js")
-// const Review = require("./models/review");
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/Auctions')
-    .then(()=> {
-        console.log("Mongo Connection open");
-    })
-    .catch(err => {
-        console.log("Mongo connection error");
-        console.log(err);
-    })
-
+mongoose.connect('mongodb://127.0.0.1:27017/Auctions', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+  .then(() => {
+    console.log("Mongo Connection open");
+  })
+  .catch((err) => {
+    console.log("Mongo connection error");
+    console.log(err);
+  });
 
 app.set("view engine", "ejs");
 app.engine("ejs", ejsMate);
 app.set("views", path.join(__dirname, "/views"));
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/listings", listingRoutes);
-app.use("/listings/:id/bids", bidRoutes);
 
 app.use(session({
-  secret: 'thisshouldbeabettersecret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7
-  }
-  
-}))
+    secret: 'thisshouldbeabettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    }
+  }))
 
 app.use(flash());
 
@@ -56,10 +55,10 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get("/", (req, res) => {
-    res.render("home.ejs");
-});
- 
+app.use("/listings", listingRoutes);
+app.use("/listings/:id/bids", bidRoutes);
+
 app.listen(3000, () => {
-    console.log("Tiripo pa port 3000");
-});
+    console.log("Takamirira hedu paPort 3000");
+})
+
